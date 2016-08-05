6a5d3416062b7b2471777be6e461fa90fdc47d71
frankNowinski <nowinski.frank@gmail.com>
2016-08-05 16:53:38 -0400

    Done.

---
+++ hello-world.js
@@ -0,0 +1,13 @@
+function hi(){
+  return "Hello World"
+}
+
+function sum(x,y){
+  return x + y
+}
+
+function sayMyName(name) {
+  return "My name is " + name
+}
+
+module.exports = {hi, sum, sayMyName}
--- test/index.js
+++ test/index.js
@@ -1,10 +1,20 @@
 var expect = require('chai').expect
+var helloWorld = require('../hello-world.js')
 
 describe('hello-world method in hello-world.js', function () {
+
   it('must match Hello World', function(done){
-    var helloWorld = require('../hello-world')()
-    helloWorld = helloWorld.replace('\n', '')
-    expect(helloWorld).to.equal('Hello World')
+    expect(helloWorld.hi()).to.equal('Hello World')
+    done()
+  })
+
+  it('adds to numbers together', function(done) {
+    expect(helloWorld.sum(1,2)).to.equal(3)
+    done()
+  })
+
+  it('says my name', function(done) {
+    expect(helloWorld.sayMyName("Frank")).to.equal("My name is Frank")
     done()
   })
 })
